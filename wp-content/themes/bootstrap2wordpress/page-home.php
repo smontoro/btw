69d15f88c95769ffd6f495631427540cdfeb98fb
smontoro <sarahjmontoro@gmail.com>
2017-12-09 00:26:35 -0800

    added more dynamic features

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -7,6 +7,8 @@
 $prelaunch_price   = get_post_meta( 9, 'prelaunch_price', true );
 $launch_price   = get_post_meta( 9, 'launch_price', true );
 $final_price   = get_post_meta( 9, 'final_price', true );
+$course_url 	= get_post_meta( 9, 'course_url', true);
+$button_text	= get_post_meta( 9, 'button_text', true);
 
 get_header(); ?>
 
@@ -38,7 +40,7 @@ get_header(); ?>
 								<span><?php echo $final_price; ?></span>
 							</div><!--price-->
 						</div><!--price-timeline-->
-						<button class="btn btn-lg btn-danger" href="#" role="button"><a>Enroll &raquo;</a></button>
+						<button class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>" role="button"><a><?php echo $button_text; ?></a></button>
 					</div><!--col-->
 
 				</div><!--row-->
