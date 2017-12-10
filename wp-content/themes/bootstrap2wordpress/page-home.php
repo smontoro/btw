34b91f835973947cfc53efced52cfc419b4f0541
smontoro <sarahjmontoro@gmail.com>
2017-12-09 17:45:41 -0800

    section features

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -25,6 +25,10 @@ $who_feature_image = get_field('income_feature_image');
 $who_feature_title = get_field('who_section_title');
 $who_section_body = get_field('who_section_body');
 
+$features_section_image = get_field('features_section_image');
+$features_section_title = get_field('features_section_title');
+$features_section_body = get_field('features_section_body');
+
 get_header(); ?>
 
 <!--=============HERO===================-->
@@ -135,36 +139,35 @@ get_header(); ?>
 	<!--==================COURSE FEATURES================-->
 	<section id="course-features">
 		<div class="container">
+
 			<div class="section-header">
-				<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-rocket.png" alt="Rocket">
-				<h2>Course Features</h2>
+
+				<!--If user uploaded an image -->
+				<?php if( !empty($features_section_image)) : ?>
+					<img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
+				<?php endif; ?>
+
+				<h2><?php echo $features_section_title; ?></h2>
+
+				<!-- If user added body text -->
+				<?php if ( !empty($features_section_body) ) : ?>
+					<p class="lead"><?php echo $features_section_body; ?></p>
+				<?php endif; ?>
+
 			</div><!-- section header -->
 
 			<div class="row">
-				<div class="col-sm-2">
-					<i class="ci ci-computer"></i>
-					<h4>Lifetime access to 80+ leactures</h4>
-				</div><!-- end col-->
-				<div class="col-sm-2">
-					<i class="ci ci-watch"></i>
-					<h4>10+ hours of HD video content</h4>
-				</div><!-- end col-->
-				<div class="col-sm-2">
-					<i class="ci ci-calendar"></i>
-					<h4>30-day money back guarantee</h4>
-				</div><!-- end col-->
-				<div class="col-sm-2">
-					<i class="ci ci-community"></i>
-					<h4>Access to a community of like-minded students</h4>
-				</div><!-- end col-->
-				<div class="col-sm-2">
-					<i class="ci ci-instructor"></i>
-					<h4>Direct access to the instructor</h4>
-				</div><!-- end col-->
-				<div class="col-sm-2">
-					<i class="ci ci-device"></i>
-					<h4>Accessible content on your mobile devices</h4>
-				</div><!-- end col-->
+
+				<?php $loop = new WP_Query( array('post_type' => 'course_feature', 'orderby' => "post_id", "order" => 'ASC') ); ?>
+
+				<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+
+					<div class="col-sm-2">
+						<i class="<?php the_field('course_feature_icon'); ?>"></i>
+						<h4><?php the_title(); ?></h4>
+					</div><!-- end col-->
+
+				<?php endwhile; ?>
 				
 			</div><!--end row-->
 		</div><!-- container-->
